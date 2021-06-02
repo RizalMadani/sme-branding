0fde318133b424805e58fb728c3252324046a4e8
RizalMadani <rizalmadani70@gmail.com>
2021-06-02 13:49:37 +0700

    Tambah library Alert

---
+++ application/libraries/Alert.php
@@ -0,0 +1,103 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * **Alert Manager**
+ * 
+ * Class untuk men-set dan menampilkan alert ke view
+ * dengan lebih praktis.
+ * Penggunaan alert mengikuti tema dasbor yang dipakai.
+ * 
+ * @author Rizal M
+ */
+class Alert {
+
+	/**
+	 * Penyimpanan semua alert yang ingin ditampilkan
+	 * 
+	 * @var array
+	 */
+	private $_alerts;
+
+	public function __construct()
+	{
+		// Key array adalah nama class alert
+		// dari Urora theme dashboard
+		$this->_alerts = array(
+			'danger'  => array(),
+			'primary' => array(),
+			'warning' => array(),
+			'info'    => array(),
+		);
+	}
+
+	/**
+	 * Set alert danger
+	 * 
+	 * @param string|array  $pesan  Pesan yang ingin ditampilkan
+	 */
+	public function alertDanger($pesan)
+	{
+		$this->_setAlert($pesan, 'danger');
+	}
+
+	/**
+	 * Set alert success
+	 * 
+	 * Menggunakan alert jenis `primary` untuk alert success
+	 * sesuai dg Urora theme dasboard
+	 * 
+	 * @param string|array  $pesan  Pesan yang ingin ditampilkan
+	 */
+	public function alertSuccess($pesan)
+	{
+		$this->_setAlert($pesan, 'primary');
+	}
+
+	/**
+	 * Set alert warning
+	 * 
+	 * @param string|array  $pesan  Pesan yang ingin ditampilkan
+	 */
+	public function alertWarning($pesan)
+	{
+		$this->_setAlert($pesan, 'warning');
+	}
+
+	/**
+	 * Set alert info
+	 * 
+	 * @param string|array  $pesan  Pesan yang ingin ditampilkan
+	 */
+	public function alertInfo($pesan)
+	{
+		$this->_setAlert($pesan, 'info');
+	}
+
+	/**
+	 * Tampilkan semua alert ke view
+	 * 
+	 * @return view
+	 */
+	public function tampilkan()
+	{
+		$CI =& get_instance();
+
+		return $CI->load->view('alert/alert', array('alerts' => $this->_alerts));
+	}
+
+	/**
+	 * Set alert danger
+	 * 
+	 * @param string|array  $pesan  Pesan yang ingin ditampilkan
+	 */
+	private function _setAlert($pesan, $jenis)
+	{
+		if (is_array($pesan)) {
+			$this->_alerts[$jenis] += $pesan;
+		}
+		else {
+			array_push($this->_alerts[$jenis], $pesan);
+		}
+	}
+}
