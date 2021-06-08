31a90cfcbf4fb44cb792caab38ef2384f7c119a9
RizalMadani <rizalmadani70@gmail.com>
2021-06-08 23:16:56 +0700

    Update favicon

--- application/views/templates/dasbor/header.php
+++ application/views/templates/dasbor/header.php
@@ -7,7 +7,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
     <title><?= ucfirst($title ?? $this->session->level).' - SME Branding'  ?></title>
 
-    <link rel="shortcut icon" href="assets/favicons/favicon-32x32.png">
+    <link rel="shortcut icon" href="<?= base_url(); ?>assets/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="<?= base_url(); ?>assets/favicons/android-icon-192x192.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?= base_url(); ?>assets/favicons/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= base_url(); ?>assets/favicons/apple-icon-180x180.png">
 
     <!--Morris Chart CSS -->
     <link rel="stylesheet" href="<?= base_url(); ?>assets/admin/plugins/fullcalendar/vanillaCalendar.css"/>
